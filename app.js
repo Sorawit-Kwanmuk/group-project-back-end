353cb5d1516b226a523ee1f8b6988fba0503a87f
Sorawit_Kwanmuk <sorawit_kwanmuk@outlook.com>
2021-10-20 10:45:21 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//สร้าง table จาก models
+// const { sequelize } = require('./models');
+// sequelize.sync({
+//   force: true,
+// });
+
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const passport = require('passport');
+const app = express();
+app.use(passport.initialize());
+app.use(express.json());
+app.use(cors());
+app.use('/public', express.static('public'));
+
+//path not found handling middleware
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: 404,
+    message: 'Page not found',
+  });
+});
+
+//error handling middleware
+app.use(errorController);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
