dd3e19e77325a5a401c0f0d440fab5d770a05107
win11264 <winthitisan@gmail.com>
2021-10-28 02:06:47 +0700

    completed Nodemailer

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ const commentRoute = require("./route/commentRoute");
 const quizRoute = require("./route/quizRoute");
 const myCourseRoute = require("./route/myCourseRoute");
 const questionRoute = require("./route/questionRoute");
+const nodemailerRoute = require("./route/nodemailerRoute");
 // var bodyParser = require("body-parser");
 
 // app.use(bodyParser.json());
@@ -58,6 +59,7 @@ app.use("/comment", commentRoute);
 app.use("/quiz", quizRoute);
 app.use("/mycourse", myCourseRoute);
 app.use("/question", questionRoute);
+app.use("/mail", nodemailerRoute);
 
 const uploadPromise = utils.promisify(cloudinary.uploader.upload);
 //path not found handling middleware
