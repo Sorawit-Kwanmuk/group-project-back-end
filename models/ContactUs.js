87809f758e72a1b689449c7d47c36029b2f47915
win11264 <winthitisan@gmail.com>
2021-11-01 11:50:31 +0700

    edit user + contactUs models

---
+++ models/ContactUs.js
@@ -0,0 +1,47 @@
+module.exports = (sequelize, DataTypes) => {
+  const ContactUs = sequelize.define(
+    "ContactUs",
+    {
+      map: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      phoneNo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      facebook: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      twitter: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      youtube: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      line: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      underscored: true,
+      paranoid: true,
+    }
+  );
+
+  return ContactUs;
+};
